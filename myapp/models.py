e354f744be0a9b75747c7e61ea3146178fb504b4
Pranjal Singh <pranjal366@gmail.com>
2021-10-05 21:33:23 +0530

    ML integrated

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
 # Create your models here.
+
+GENDER_CHOICES = [
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+]
+
+MARRIED_CHOICES = [
+    ('No', 'No'),
+    ('Yes', 'Yes'),
+]
+
+SPENDING_CHOICES = [
+    ('Low', 'Low'),
+    ('Average', 'Average'),
+    ('High', 'High'),
+]
+
+GRADUATED_CHOICES = [
+    ('No', 'No'),
+    ('Yes', 'Yes'),
+]
+
+PROFESSION_CHOICES = [
+    ('Healthcare', 'Healthcare'),
+    ('Engineer', 'Engineer'),
+    ('Lawyer', 'Lawyer'),
+    ('Doctor', 'Doctor'),
+    ('Entertainment', 'Entertainment'),
+    ('Artist', 'Artist'),
+    ('Executive', 'Executive'),
+    ('Homemaker', 'Homemaker'),
+    ('Marketing', 'Marketing'),
+]
+
+CATEGORY_CHOICES = [
+    ('Var_1_0', '1'),
+    ('Var_1_1', '2'),
+    ('Var_1_2', '3'),
+    ('Var_1_3', '4'),
+    ('Var_1_4', '5'),
+    ('Var_1_5', '6'),
+    ('Var_1_6', '7'),
+    ('Var_1_7', '8'),
+    ('Var_1_8', '8'),
+]
+
+
+class UserInfo(models.Model):
+    customer_id = models.CharField(max_length=6)
+    gender = models.CharField(max_length=7, choices=GENDER_CHOICES)
+    age = models.CharField(max_length=4)
+    married = models.CharField(max_length=4, choices=MARRIED_CHOICES)
+    graduated = models.CharField(max_length=4, choices=GRADUATED_CHOICES)
+    profession = models.CharField(max_length=15, choices=PROFESSION_CHOICES)
+    work_experience = models.CharField(max_length=3)
+    spending = models.CharField(max_length=8, choices=SPENDING_CHOICES)
+    family_size = models.CharField(max_length=3)
+    category = models.CharField(max_length=8, choices=CATEGORY_CHOICES, default='None')
+    segment = models.CharField(max_length=4, blank=True, default='None')
+
+    def __str__(self):
+        return self.customer_id
--- myapp/views.py
+++ myapp/views.py
@@ -1,17 +1,49 @@
 from django.shortcuts import render
-
+from myapp.segmentation import check
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 import myapp
 
+changer = {'Gender_1': 'Male', 'Gender_0': 'Female', 'Ever_Married_1': 'Yes', 'Ever_Married_0': 'No',
+           'Ever_Married_1': 'Yes', 'Graduated_1': 'Yes', 'Graduated_0': 'No', 'Profession_5': 'Healthcare',
+		   'Profession_2': 'Engineer', 'Profession_7': 'Lawyer', 'Profession_3': 'Entertainment',
+		   'Profession_0': 'Artist', 'Profession_1': 'Doctor', 'Profession_4': 'Executive',
+		   'Profession_6': 'Homemaker', 'Profession_8': 'Marketing', 'Var_1_0': 1, 'Var_1_1': 2, 'Var_1_2': 3,
+		   'Var_1_3': 4, 'Var_1_4': 5, 'Var_1_5': 6, 'Var_1_6': 7, 'Var_1_7': 8, 'Spending_Score_2': 'Low',
+		   'Spending_Score_0': 'Average', 'Spending_Score_1': 'High'}
+
+
 # Create your views here.
 def index(request):
-	return render(request,'index.html')
+    return render(request, 'index.html')
+
 
 def service(request):
-	return render(request,'services.html')
+	if request.method == 'POST':
+		segment = check(request.POST)
+		customer_id = request.POST.get('ID')
+		age = request.POST.get('Age')
+		gender = changer[request.POST.get('gender')]
+		married = changer[request.POST.get('married')]
+		graduated = changer[request.POST.get('graduated')]
+		profession = changer[request.POST.get('profession')]
+		work_experience = request.POST.get('Work_Experience')
+		spending = changer[request.POST.get('spending')]
+		family_size = request.POST.get('Family_Size')
+		category = changer[request.POST.get('category')]
+		ml_model = UserInfo(customer_id=customer_id, age=age, gender=gender, married=married, graduated=graduated,
+							profession=profession, work_experience=work_experience, spending=spending, family_size=family_size,
+							category=category, segment=segment)
+		ml_model.save()
+		return HttpResponseRedirect(reverse('result'))
+	return render(request, 'services.html')
+
 
 def result(request):
-	return render(request,'results.html')
+	data = UserInfo.objects.all().order_by('-id')
+	return render(request, 'results.html', {'data': data})
+
 
 def analytics(request):
-	return render(request,'analytics.html')
+    return render(request, 'analytics.html')
